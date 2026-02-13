be964c73e2063b00e0c8deda178a772056a9a35f
muhhlmy <muhhlmy@gmail.com>
2026-02-13 10:22:09 +0700

    Menambahkan fungsionalitas otentikasi pengguna dengan JWT, termasuk rute untuk signin dan inisialisasi admin, serta konfigurasi MongoDB.

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app";
+import { error } from "node:console";
+
+dotenv.config();
+
+const PORT = process.env.PORT || "5001";
+const MONGO_URI = process.env.MONGO_URI || "no-mongo-uri";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+    app.listen(PORT, () => {
+      console.log(`Server is Running on PORT ${PORT}`);
+    });
+  })
+  .catch((error) => console.error("Error connecting to MongoDB", error));
