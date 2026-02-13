c3c052aa73fb86ae0a865bcab6dae3198f6d780f
muhhlmy <muhhlmy@gmail.com>
2026-02-13 10:32:03 +0700

    Menambahkan middleware otentikasi dan file .gitignore untuk mengabaikan direktori node_modules

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
+import { authenticate } from "./middlewares/auth.middleware";
 
 const app = express();
 
@@ -8,8 +9,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Sporton Backend API is Running");
+});
+
+app.get("/test-middleware", authenticate, (req, res) => {
+  res.send("Hore! kamu bisa mengaksesnya karena kamu pakai token!");
 });
 
 export default app;
